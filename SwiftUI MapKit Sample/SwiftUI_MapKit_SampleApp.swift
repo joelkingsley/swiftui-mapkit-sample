06c902e980eb72746fc2b33f022f4a4fc364297a
Joel Kingsley <joelkingsley.r@gmail.com>
2022-07-04 19:39:35 +0530

    Initial Commit

---
+++ SwiftUI MapKit Sample/SwiftUI_MapKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapKit_SampleApp.swift
+//  SwiftUI MapKit Sample
+//
+//  Created by Joel Kingsley on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
